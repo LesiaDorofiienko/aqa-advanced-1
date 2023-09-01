489d045792a9b24c99e52ba073e3efbc3767596a
LesiaD <lesiadorofienko@gmail.com>
2023-09-01 15:50:40 +0300

    change cylinder area to volume

--- js_basics/task5.js
+++ js_basics/task5.js
@@ -5,10 +5,10 @@ const length = 35;
 const width = 12;
 const height = 10;
 
-const circleArea = Math.PI * (radius ^ 2);
+const circleArea = (Math.PI * (radius ^ 2)).toFixed(2);
 const rectangleArea = length * width;
-const cylinderArea = Math.PI * (radius^2) * height;
+const cylinderVolume = (Math.PI * (radius^2) * height).toFixed(2);
 
 console.log(chalk.red('Circle area is: ', circleArea));
 console.log(chalk.green('Rectangle area is: ', rectangleArea));
-console.log(chalk.blueBright('Cylinder area is: ', cylinderArea));
+console.log(chalk.blueBright('Cylinder volume is: ', cylinderVolume));
