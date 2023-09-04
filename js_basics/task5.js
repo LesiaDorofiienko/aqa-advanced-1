c5104bbc5f52c0a9fb56fa086869fb9671be3a79
LesiaD <lesiadorofienko@gmail.com>
2023-09-04 11:15:18 +0300

    pr changes

--- js_basics/task5.js
+++ js_basics/task5.js
@@ -4,10 +4,12 @@ const radius = 4;
 const length = 35;
 const width = 12;
 const height = 10;
+const prSquare = Math.PI * (radius ** 2)
 
-const circleArea = (Math.PI * (radius ^ 2)).toFixed(2);
+const circleArea = (prSquare).toFixed(2);
 const rectangleArea = length * width;
-const cylinderVolume = (Math.PI * (radius^2) * height).toFixed(2);
+const cylinderVolume = (prSquare * height).toFixed(2);
+
 
 console.log(chalk.red('Circle area is: ', circleArea));
 console.log(chalk.green('Rectangle area is: ', rectangleArea));
